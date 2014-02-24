9199873a48c7119a01ff5c24331bea5c82661239
Ramiro Araujo <rama.araujo@gmail.com>
2014-02-24 14:45:25 -0300

    finished implementation

--- workflow/feedback.rb
+++ workflow/feedback.rb
@@ -4,37 +4,44 @@
 require_relative 'bundle/bundler/setup'
 require 'alfred'
 
+filter = ARGV[0].to_s.downcase
 
-
+modes = `./setresx --modes`.chomp("\n").split("\n").map do |line|
+  mode = {}
+  line[7..-2].split(', ').each { |opt| opt = opt.split(/\s*=\s*/); mode[opt[0]] = opt[1] }
+  mode['width'] = mode['resolution'].split('x')[0]
+  mode['height'] = mode['resolution'].split('x')[1]
+  mode['dpi'] = mode['scale'] == '2.0' ? 'HiDPI' : 'normal resolution'
+  mode
+end
 
 Alfred.with_friendly_error do |alfred|
   fb = alfred.feedback
 
-  # add a file feedback
-  fb.add_file_item(File.expand_path '~/Applications/')
+  modes.each do |mode|
+    unless filter =~ /^\s*$/
+      next unless mode['resolution'] =~ /#{filter}/ || mode['dpi'] =~ /#{filter}/
+    end
 
-  # add an arbitrary feedback
-  fb.add_item({
-    :uid      => '',
-    :title    => 'Just a Test',
-    :subtitle => 'feedback item',
-    :arg      => 'A test feedback Item',
-    :valid    => 'yes',
-  })
-  
-  # add an feedback to test rescue feedback
-  fb.add_item({
-    :uid          => '',
-    :title        => 'Rescue Feedback Test',
-    :subtitle     => 'rescue feedback item',
-    :arg          => '',
-    :autocomplete => 'failed',
-    :valid        => 'no',
-  })
+    fb.add_item({
+                    :uid => '',
+                    :title => "Switch to #{mode['resolution']} at #{mode['dpi']}",
+                    :subtitle => "Change resolution to #{mode['width']} width and #{mode['height']} height at #{mode['dpi']} mode",
+                    :arg => mode['resolution'],
+                    :valid => 'yes',
+                })
 
-  if ARGV[0].eql? 'failed'
-    alfred.with_rescue_feedback = true
-    raise Alfred::NoBundleIDError, 'Wrong Bundle ID Test!'
+  end
+
+  if fb.to_xml().to_s == '<items/>'
+    fb.add_item({
+                    :uid => '',
+                    :title => 'No results for your search',
+                    :subtitle => 'No screen resolution found for your input search',
+                    :arg => '',
+                    :valid => 'no',
+                })
+
   end
 
   puts fb.to_xml()
--- workflow/set-resolution.rb
+++ workflow/set-resolution.rb
@@ -1,3 +1,13 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 
+width, height = ARGV[0].split 'x'
+
+result = system("./setresx -w #{width} -h #{height}");
+
+unless result
+  `/usr/bin/afplay /System/Library/Sounds/Funk.aiff`
+  exit 1
+end
+
+print ARGV[0]
